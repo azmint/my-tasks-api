0d6a46d60c0840da49de26185a6b25c8358aec4e
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-05 19:10:45 +0900

    add sample files

---
+++ src/sample.py
@@ -0,0 +1,19 @@
+"""Sample"""
+
+
+class Sample:
+    """Sample"""
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def get_name(self) -> str:
+        """sample
+
+        :return: name
+        """
+        return self.name
+
+    def print_name(self):
+        """print name"""
+        print(self.name)
---
+++ test/test_sample.py
@@ -0,0 +1,16 @@
+"""Test sample.py"""
+
+
+class TestSample:
+    """Test Sample"""
+
+    def test_get_name(self):
+        """Test get_name"""
+        from sample import Sample
+
+        sample: Sample = Sample("sample")
+        assert sample.get_name() == "sample"
+
+    def test_print_name(self):
+        """Test print_name"""
+        pass
