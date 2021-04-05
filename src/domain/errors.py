017f7b4117ff7878265638395489b75975cbfd7a
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-05 20:07:19 +0900

    add basic domain class

--- src/domain/errors.py
+++ src/domain/errors.py
@@ -1,15 +1,15 @@
 from typing import Optional, Dict, Any
 
 
-class ApplicationError(Exception):
+class BaseError(Exception):
     """アプリケーションレイヤーのエラー
 
     コアロジックでのエラー
     """
-    def __init__(self,
-                 message: str,
-                 values: Dict[str, Any] = None,
-                 cause: Optional[Exception] = None):
+
+    def __init__(
+        self, message: str, values: Dict[str, Any] = None, cause: Optional[Exception] = None
+    ):
         """
 
         :param message: エラーメッセージ
@@ -20,3 +20,17 @@ class ApplicationError(Exception):
         self.message: str = message
         self.values: Dict[str, Any] = values if values else {}
         self.cause: Optional[Exception] = cause
+
+
+class ApplicationError(BaseError):
+    """アプリケーションレイヤーのエラー
+
+    コアロジックでのエラー
+    """
+
+
+class InfrastructureError(BaseError):
+    """インフラストラクチャーレイヤーのエラー
+
+    依存しているライブラリやサービスでのエラーは、このエラーに変換してraiseする。
+    """
