6696ec57710b56a078ffd0fa1e7a83bfe1a14141
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-05 20:07:19 +0900

    add basic domain class

---
+++ src/domain/application_error.py
@@ -0,0 +1,22 @@
+from typing import Optional, Dict, Any
+
+
+class ApplicationError(Exception):
+    """アプリケーションレイヤーのエラー
+
+    コアロジックでのエラー
+    """
+    def __init__(self,
+                 message: str,
+                 values: Dict[str, Any] = None,
+                 cause: Optional[Exception] = None):
+        """
+
+        :param message: エラーメッセージ
+        :param values: 付加情報
+        :param cause: 原因となったエラー(DBアクセス時に発生した依存ライブラリのエラーなど)
+        """
+        super().__init__(message)
+        self.message: str = message
+        self.values: Dict[str, Any] = values if values else {}
+        self.cause: Optional[Exception] = cause
