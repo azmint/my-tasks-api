97d50e31b8f6a0f9cadd9ad71f678738b0374412
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-10 12:49:07 +0900

    convert to dataclass

--- src/domain/task/task_id.py
+++ src/domain/task/task_id.py
@@ -1,5 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class TaskID(str):
     """タスクID"""
 
-    def __repr__(self) -> str:
-        return f"{self.__class__.__module__}.{self.__class__.__qualname__}" + f"('{self}')"
+    value: str
