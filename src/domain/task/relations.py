63ca223f04f5ae977bc7fe661b02d3a6b87e1e74
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-10 14:27:30 +0900

    add task

---
+++ src/domain/task/relations.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from enum import Enum, auto
+from typing import List
+
+from domain.task.task_id import TaskID
+
+
+class Type(Enum):
+    """関係の種類"""
+
+    # 親
+    PARENT = auto()
+    # 子
+    CHILD = auto()
+
+
+@dataclass(frozen=True)
+class Relation:
+    """他タスクとの関係"""
+
+    # 関係種別
+    type: Type
+    # 関係タスク
+    task_id: TaskID
+
+
+@dataclass
+class Relations:
+    """関係の一覧"""
+
+    values: List[Relation]
