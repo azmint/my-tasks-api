0ea104b8c9378e6933facf1ff03d846655110d65
Yuki Roppongi <az0.mint9@gmail.com>
2021-04-10 12:42:18 +0900

    fix unittest

--- src/domain/task/task_id.py
+++ src/domain/task/task_id.py
@@ -1,9 +1,5 @@
 class TaskID(str):
     """タスクID"""
 
-    #
-    # def __repr__(self) -> str:
-    #     return (
-    #             f"{self.__class__.__module__}.{self.__class__.__qualname__}"
-    #             + f"({self})"
-    #     )
+    def __repr__(self) -> str:
+        return f"{self.__class__.__module__}.{self.__class__.__qualname__}" + f"('{self}')"
